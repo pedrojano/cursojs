c2161ed3b3f3ec968212261dc63500cf81510def
Pedro Jano <pedrino022@gmail.com>
2024-12-11 18:14:37 -0300

    Foi corrigido o bug da aula 11, pois não estva aparecendo o resultado

--- aula-11/aula-11.js
+++ aula-11/aula-11.js
@@ -1,10 +1,10 @@
 
 function soma() {
-    let tn1 = document.getElementById('txtn1');
-    let tn2 = document.getElementById('txtn2');
-    let resu = document.getElementById('res');
-    let n1 = Number(tn1);
-    let n2 = Number(tn2);
-    let s = n1 + n2;
-    resu.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${s}`;
+    let txtn1 = document.getElementById('txtn1');
+    let txtn2 = document.getElementById('txtn2');
+    let resu = document.getElementById('resu')
+    let n1 = Number(txtn1.value);
+    let n2 = Number(txtn2.value);
+    let soma = n1 + n2;
+    resu.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${soma}`;
 }
